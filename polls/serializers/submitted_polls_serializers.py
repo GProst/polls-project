e16e5ae329340d9eb7c89d5d3356616bf5b57380
GProst <gprostmail@gmail.com>
2020-06-30 00:36:36 +0200

    Slightly improve API output

--- polls/serializers/submitted_polls_serializers.py
+++ polls/serializers/submitted_polls_serializers.py
@@ -1,6 +1,18 @@
 from rest_framework import serializers
 
+from ..models import Question
+
 class SubmittedPollSerializer(serializers.BaseSerializer):
+  @staticmethod
+  def get_answer_representation(question, user_id):
+    answer = question.answers.get(user_id=user_id).answer
+    if question.type == Question.TEXT_TYPE:
+      return answer
+    elif question.type == Question.SINGLE_CHOICE_TYPE:
+      return int(answer)
+    else:
+      return [int(a) for a in answer.split(',')]
+
   def to_representation(self, poll):
     user_id = self.context['view'].request.query_params['user_id']
 
@@ -18,6 +30,6 @@ class SubmittedPollSerializer(serializers.BaseSerializer):
           'id': choice.id,
           'text': choice.text
         } for choice in question.choices.all()],
-        'answer': question.answers.get(user_id=user_id).answer
+        'answer': self.get_answer_representation(question, user_id)
       } for question in poll.questions.all()]
     }
